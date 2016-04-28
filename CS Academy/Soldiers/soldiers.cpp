4910aeb7f3181cd440877fc8d1f75f586dbacd1d
Marcos Kawakami <marcoskwkm@gmail.com>
2016-04-28 16:51:30 -0300

    solved Soldiers

--- CS Academy/Soldiers/soldiers.cpp
+++ CS Academy/Soldiers/soldiers.cpp
@@ -6,92 +6,142 @@ using namespace std;
 typedef long long lint;
 typedef pair<int,int> pii;
 typedef pair<lint,lint> pll;
+typedef pair<pii, int> ppi;
 
 const int INF = 0x3f3f3f3f;
 const lint LINF = 0x3f3f3f3f3f3f3f3fll;
 const int MAXN = (int)1e5 + 10;
 const int MOD = (int)1e9 + 7;
 
-vector<pii> v[MAXN];
-vector<pii> same;
-int sol[MAXN];
-int sol2[MAXN];
+pii in[MAXN];
+map< pii, vector<int> > mp;
+int ans[MAXN];
+int group[MAXN];
+vector<int> groups[MAXN];
+int paired[MAXN];
+int tem[MAXN];
+bool foi[MAXN];
+int n;
+
+bool check_valid() {
+    vector<pii> p1, p2;
+    for (int i = 0; i < n; i++) {
+        if (ans[i]) p2.push_back(in[i]);
+        else p1.push_back(in[i]);
+    }
+    sort(p1.begin(), p1.end());
+    sort(p2.begin(), p2.end());
+    int i = 0, j = 0;
+    int s1 = p1.size(), s2 = p2.size();
+    while (i < s1 || j < s2) {
+        if (i < s1 && p1[i] == pii(i, s2 - j)) {
+            i++;
+            continue;
+        }
+        if (j < s2 && p2[j] == pii(j, s1 - i)) {
+            j++;
+            continue;
+        }
+        return 0;
+    }
+    return 1;
+}
 
 int main() {
     int t;
     for (scanf("%d", &t); t--;) {
-        int n;
-        int hi = 0;
+        mp.clear();
         scanf("%d", &n);
-        int ans = 2;
-        same.clear();
+        int s1 = 0, s2 = 0;
+        int q = 2;
+        for (int i = 0; i < n; i++) {
+            paired[i] = -1;
+            tem[i] = -1;
+        }
         for (int i = 0; i < n; i++) {
             int a, b;
             scanf("%d%d", &a, &b);
-            if (!v[a].empty() && v[a].back().first == b) {
-                ans = (ans << 1) % MOD;
-                same.push_back(pii(v[a].back().second, i));
+            in[i] = pii(a, b);
+            if (tem[a] != -1) s2 = max(s2, a + 1);
+            else s1 = max(s1, a + 1);
+            tem[a] = i;
+            if (mp.find(pii(a, b)) != mp.end()) {
+                int prv = mp[pii(a, b)].back();
+                paired[prv] = i;
+                paired[i] = prv;
+                // printf("pair %d - %d\n", i, prv);
+                q = (q << 1) % MOD;
             }
-            v[a].push_back(pii(b, i));
-            hi = max(hi, a);
+            mp[pii(a, b)].push_back(i);
+            groups[i].clear();
         }
-        printf("%d\n", ans);
-        int s1 = hi + 1;
-        int s2 = n - s1;
-        int i = 0, j = 0;
-        while (i < s1 || j < s2) {
-            bool found = 0;
-            if (i < s1) {
-                for (pii &p: v[i]) {
-                    if (p.first == s2 - j) {
-                        printf("take (%d, %d) to 0\n", i, p.first);
-                        sol[p.second] = 0;
-                        swap(p, v[i].back());
-                        v[i].pop_back();
-                        found = 1;
-                        i++;
-                        break;
-                    }
-                }
+        int i1 = 0, i2 = 0;
+        int cur_group = 0;
+        while (i1 < s1 || i2 < s2) {
+            int ii = -1, ij = -1;
+            vector<int> &vi = mp[pii(i1, s2 - i2)];
+            vector<int> &vj = mp[pii(i2, s1 - i1)];
+            if (i1 < s1 && !vi.empty()) ii = vi.back();
+            if (i2 < s2 && !vj.empty()) ij = vj.back();
+            assert(ii != -1 || ij != -1);
+            if (ii != -1 && ij != -1) {
+                if (s1 == s2) cur_group++;
+                ans[ij] = 1;
+                group[ij] = cur_group;
+                groups[cur_group].push_back(ij);
+                vj.pop_back();
+                i2++;
             }
-            if (!found && j < s2) {
-                for (pii &p: v[j]) {
-                    if (p.first == s1 - i) {
-                        printf("take (%d, %d) to 1\n", j, p.first);
-                        sol[p.second] = 1;
-                        swap(p, v[j].back());
-                        v[j].pop_back();
-                        found = 1;
-                        j++;
-                        break;
-                    }
-                }
+            else if (ii != -1) {
+                ans[ii] = 0;
+                group[ii] = cur_group;
+                groups[cur_group].push_back(ii);
+                vi.pop_back();
+                i1++;
             }
-            assert(found);
+            else {
+                ans[ij] = 1;
+                group[ij] = cur_group;
+                groups[cur_group].push_back(ij);
+                vj.pop_back();
+                i2++;
+            }
         }
-        for (int i = 0; i < n; i++)
-            sol2[i] = sol[i] ^ 1;
-        for (pii p: same) {
-            if (sol[p.first])
-                swap(sol[p.first], sol[p.second]);
-            else
-                swap(sol2[p.first], sol2[p.second]);
-        }
+        for (int i = 1; i < cur_group; i++)
+            q = (q << 1) % MOD;
+        printf("%d\n", q);
 
-        bool take_first = 1;
+        // printf("before: ");
+        // for (int i = 0; i < n; i++) printf("%d", ans[i]);
+        // printf("\n");
 
+        for (int i = 0; i <= cur_group; i++) {
+            foi[i] = 0;
+            sort(groups[i].begin(), groups[i].end());
+        }
         for (int i = 0; i < n; i++) {
-            if (sol2[i] < sol[i]) {
-                take_first = 0;
-                break;
+            if (foi[group[i]]) continue;
+            foi[group[i]] = 1;
+            int first = -1;
+            for (int j: groups[group[i]]) {
+                if (paired[j] != -1) {
+                    if (paired[j] < j) continue;
+                    ans[j] = 0;
+                    ans[paired[j]] = 1;
+                }
+                else {
+                    if (first == -1) first = ans[j];
+                    ans[j] ^= first;
+                }
             }
-            if (sol[i] < sol2[i]) break;
         }
-        
-        for (int i = 0; i < n; i++) printf("%d", sol[i]);
-        printf("\n");        
-        
-        for (int i = 0; i <= hi; i++) v[i].clear();
+
+        // printf("after: ");
+        for (int i = 0; i < n; i++)
+            printf("%d", ans[i]);
+        printf("\n");
+
+        assert(check_valid());
     }        
     return 0;
 }
