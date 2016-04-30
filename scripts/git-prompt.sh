cdfd5fcb71cb8a1349b7cd60e25eeed42a415121
Marcos Kawakami <marcoskwkm@gmail.com>
2016-04-29 22:34:30 -0300

    Add scripts

---
+++ scripts/git-prompt.sh
@@ -0,0 +1,9 @@
+# Terminal shows current git branch
+
+parse_git_branch() {
+
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+
+}
+
+export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
