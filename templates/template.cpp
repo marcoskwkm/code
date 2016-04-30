a9c56db9c8d93646bf1943c5e063a0bd33614e15
Marcos Kawakami <marcoskwkm@gmail.com>
2016-04-29 22:21:20 -0300

    Add templates

---
+++ templates/template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(args...) fprintf(stderr,args)
+
+typedef long long lint;
+typedef pair<int,int> pii;
+typedef pair<lint,lint> pll;
+
+const int INF = 0x3f3f3f3f;
+const lint LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main() {
+    
+    return 0;
+}
