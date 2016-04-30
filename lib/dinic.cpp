a500f6b91c5ad3984630e858b4724e57a88bce50
Marcos Kawakami <marcoskwkm@gmail.com>
2016-04-30 02:17:50 -0300

    add reset function to dinic

--- lib/dinic.cpp
+++ lib/dinic.cpp
@@ -13,26 +13,28 @@ struct Edge {
 };
 
 vector<int> adj[MAXV];
-vector<Edge> edge;
+vector<Edge> edge, s_edge;
 int ptr[MAXV], dinic_dist[MAXV];
 
 // Inserts an edge u->v with capacity c
-inline void add_edge(int u,int v,FTYPE c) {
+inline void add_edge(int u, int v, FTYPE c) {
     adj[u].push_back(edge.size());
-    edge.push_back(Edge(v,c));
+    edge.push_back(Edge(v, c));
+    s_edge.push_back(Edge(v, c));
     adj[v].push_back(edge.size());
-    edge.push_back(Edge(u,0)); // modify to Edge(u,c) if graph is non-directed
+    edge.push_back(Edge(u, 0)); // modify to Edge(u, c) if graph is non-directed
+    s_edge.push_back(Edge(u, 0));
 }
 
-bool dinic_bfs(int _s,int _t) {
-    memset(dinic_dist,-1,sizeof(dinic_dist));
+bool dinic_bfs(int _s, int _t) {
+    memset(dinic_dist, -1, sizeof(dinic_dist));
     dinic_dist[_s] = 0;
     queue<int> q;
     q.push(_s);
     while(!q.empty() && dinic_dist[_t] == -1) {
         int v = q.front();
         q.pop();
-        for(size_t a=0;a<adj[v].size();++a) {
+        for(size_t a = 0; a < adj[v].size(); ++a) {
             int ind = adj[v][a];
             int nxt = edge[ind].to;
             if(dinic_dist[nxt] == -1 && edge[ind].cap) {
@@ -44,13 +46,13 @@ bool dinic_bfs(int _s,int _t) {
     return dinic_dist[_t] != -1;
 }
 
-FTYPE dinic_dfs(int v,int _t,FTYPE flow) {
+FTYPE dinic_dfs(int v, int _t, FTYPE flow) {
     if(v == _t) return flow;
-    for(int &a = ptr[v];a<(int)adj[v].size();++a) {
+    for(int &a = ptr[v]; a < (int)adj[v].size(); ++a) {
         int ind = adj[v][a];
         int nxt = edge[ind].to;
         if(dinic_dist[nxt] == dinic_dist[v] + 1 && edge[ind].cap) {
-            FTYPE got = dinic_dfs(nxt,_t,min(flow,edge[ind].cap));
+            FTYPE got = dinic_dfs(nxt, _t, min(flow, edge[ind].cap));
             if(got) {
                 edge[ind].cap -= got;
                 edge[ind^1].cap += got;
@@ -61,17 +63,23 @@ FTYPE dinic_dfs(int v,int _t,FTYPE flow) {
     return 0;
 }
 
-FTYPE dinic(int _s,int _t) {
+FTYPE dinic(int _s, int _t) {
     FTYPE ret = 0, got;
-    while(dinic_bfs(_s,_t)) {
-        memset(ptr,0,sizeof(ptr));
-        while((got = dinic_dfs(_s,_t,FINF))) ret += got;
+    while(dinic_bfs(_s, _t)) {
+        memset(ptr, 0, sizeof(ptr));
+        while((got = dinic_dfs(_s, _t, FINF))) ret += got;
     }
     return ret;
 }
 
+// Removes all flow but keeps graph structure
+void dinic_reset() {
+    for (int i = 0; i < (int)edge.size(); i++)
+        edge[i].cap = s_edge[i].cap;
+}
+
 // Clears dinic structure
 inline void dinic_clear() {
-    for(int a=0;a<MAXV;++a) adj[a].clear();
+    for(int i = 0; i < MAXV; ++i) adj[i].clear();
     edge.clear();
 }
