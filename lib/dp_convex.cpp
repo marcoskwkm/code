b48ccdae7c63a50e8c8d708156fbfcb7c74202b4
Marcos Kawakami <marcoskwkm@gmail.com>
2016-06-13 19:41:25 -0300

    Fixed and optimized convex hull dp optimization (general case)

--- lib/dp_convex.cpp
+++ lib/dp_convex.cpp
@@ -1,24 +1,23 @@
 // Convex Hull Optimization (general case)
 // O(logn) for insertion and query
+// Test: SPOJ GOODG
 
 template< class C_TYPE, class Compare = less<C_TYPE> > struct ConvexHullOpt {
     struct Line {
         C_TYPE a, b; // a + bx
         double end_l, end_r;
         Line(){}
-        Line(C_TYPE aa, C_TYPE bb) { a = aa; b = bb; end_l = -LINF, end_r = LINF; }
-        C_TYPE get(C_TYPE x) const { return a + b*x; }
+        Line(C_TYPE aa, C_TYPE bb) { a = aa; b = bb; end_l = -1e80, end_r = 1e80; }
+        inline C_TYPE get(C_TYPE x) const { return a + b*x; }
     };
 
-    class by_slope {
-    public:
+    struct by_slope {
         bool operator()(const Line &a, const Line &b) const {
             return Compare()(a.b, b.b);
         }
     };
         
-    class by_end {
-    public:
+    struct by_end {
         bool operator()(const Line &a, const Line &b) const {
             return a.end_r < b.end_r;
         }
@@ -33,55 +32,47 @@ template< class C_TYPE, class Compare = less<C_TYPE> > struct ConvexHullOpt {
 
     void add_line(C_TYPE a, C_TYPE b) {
         Line newline(a, b);
-        auto it = set_slope.lower_bound(newline);
-        vector<Line> trash;
-        vector< pair<Line, Line> > update;
-        if (it != set_slope.end()) {
-            if (cross(*it, newline) < it->end_l) return;
-            auto it2 = it;
-            while (it2 != set_slope.end() && cross(*it2, newline) > it2->end_r) {
-                trash.push_back(*it2);
-                it2++;
+        auto itr = set_slope.lower_bound(newline);
+        auto itr2 = itr == set_slope.end() ? set_end.end() : set_end.lower_bound(*itr);
+        if (itr != set_slope.end()) {
+            if (cross(*itr, newline) < itr->end_l) return;
+            while (itr != set_slope.end() && cross(*itr, newline) > itr->end_r) {
+                itr = set_slope.erase(itr);
+                itr2 = set_end.erase(itr2);
             }
-            if (it2 != set_slope.end()) {
-                double x = cross(*it2, newline);
-                newline.end_r = x;
-                Line tmp = *it2;
-                tmp.end_l = x;
-                update.push_back(make_pair(*it2, tmp));
+            if (itr != set_slope.end()) {
+                double x = cross(*itr, newline);
+                Line tmp = *itr;
+                newline.end_r = tmp.end_l = x;
+                itr = set_slope.erase(itr);
+                itr = set_slope.insert(itr, tmp);
+                itr2 = set_end.erase(itr2);
+                itr2 = set_end.insert(itr2, tmp);
             }
         }
-        while (it != set_slope.begin()) {
-            it--;
-            double x = cross(*it, newline);
-            if (x > it->end_l) {
-                Line tmp = *it;
-                newline.end_l = x;
-                tmp.end_r = x;
-                update.push_back(make_pair(*it, tmp));
+        auto itl = itr;
+        auto itl2 = itr2;
+        while (itl != set_slope.begin()) {
+            itl--, itl2--;
+            double x = cross(*itl, newline);
+            if (x > itl->end_l) {
+                Line tmp = *itl;
+                newline.end_l = tmp.end_r = x;
+                itl = set_slope.erase(itl);
+                itl = set_slope.insert(itl, tmp);
+                itl2 = set_end.erase(itl2);
+                itl2 = set_end.insert(itl2, tmp);
                 break;
             }
-            trash.push_back(*it);
+            itl = set_slope.erase(itl);
+            itl2 = set_end.erase(itl2);
         }
-        for (const Line &line: trash) {
-            set_slope.erase(line);
-            set_end.erase(line);
-        }
-        for (const auto &upd: update) {
-            set_slope.erase(upd.first);
-            set_end.erase(upd.first);
-            set_slope.insert(upd.second);
-            set_end.insert(upd.second);
-        }
-        set_slope.insert(newline);
-        set_end.insert(newline);
+        set_slope.insert(itr, newline);
+        set_end.insert(itr2, newline);
     }
 
     C_TYPE get(C_TYPE x) {
-        if (set_end.empty()) {
-            // can't query with no lines in structure =P
-            abort();
-        }
+        if (set_end.empty()) return -LINF;
         Line dummy;
         dummy.end_r = x;
         auto it = set_end.lower_bound(dummy);
