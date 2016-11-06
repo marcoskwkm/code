f09a7faebc07fc169ec2c0cdc2b5b4a62fd433de
Marcos Kawakami <marcoskwkm@gmail.com>
2016-11-06 14:46:31 -0200

    stuff

--- Contests/Pacific Northwest 2014/a.py
+++ Contests/Pacific Northwest 2014/a.py
@@ -1,26 +1,22 @@
 import sys
 n = int(raw_input())
 for line in sys.stdin.readlines():
-    try:
-        takezero = True
-        ops = line.replace('+',';').replace('*',';').replace('-',';').replace('=',';').split(';')
-        for x in ops:
-            if x != "" and x[0] == '?':
-                if x != "?":
-                    takezero = False
-        line = line.split('=')
-        lhs, rhs = line[0], line[1]
-        found = 0
-        for d in xrange(ord('0'), ord('9') + 1):
-            if chr(d) == '0' and not takezero: continue
-            if d in line: continue
-            l = lhs.replace('?', chr(d))
-            r = rhs.replace('?', chr(d))
-            if eval(l) == eval(r):
-                found = 1
-                print chr(d)
-                break
-        if found == 0:
-            print -1
-    except:
-        continue
+    takezero = True
+    ops = line.strip()
+    for op in '+*-=':
+        ops = ops.replace(op, ';')
+    ops = ops.split(';')
+    for x in ops:
+        if x != "" and x[0] == '?' and x != "?":
+            takezero = False
+    found = 0
+    for d in xrange(ord('0'), ord('9') + 1):
+        if chr(d) == '0' and not takezero: continue
+        if chr(d) in line: continue
+        l, r = line.replace('?', chr(d)).split('=')
+        if eval(l) == eval(r):
+            found = 1
+            print chr(d)
+            break
+    if found == 0:
+        print -1
