9e998ba1f51d2a494cadb3b6dcb0cbb90e3c7362
Marcos Kawakami <marcoskwkm@gmail.com>
2016-11-26 13:39:33 -0200

    add tuple int to template

--- templates/template.cpp
+++ templates/template.cpp
@@ -6,6 +6,7 @@ using namespace std;
 typedef long long lint;
 typedef pair<int, int> pii;
 typedef pair<lint, lint> pll;
+typedef tuple<int, int, int> tiii;
 
 const int INF = 0x3f3f3f3f;
 const lint LINF = 0x3f3f3f3f3f3f3f3fll;
