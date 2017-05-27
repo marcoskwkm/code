6d004d91871f10bfb3bfcb9157994357c74df920
Marcos Kawakami <marcoskwkm@gmail.com>
2017-05-27 14:26:53 -0300

    [libv2] OrderedSet

---
+++ libv2/OrderedSet.h
@@ -0,0 +1,11 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+// Supports *find_by_order() and order_of_key()
+template<class T> using OrderedSet = tree<
+    T,
+    null_type,
+    less<T>,
+    rb_tree_tag,
+    tree_order_statistics_node_update>;
