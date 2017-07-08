a7ceef9f1e889008805616651d3e616b76b93fb2
Marcos Kawakami <marcoskwkm@gmail.com>
2017-07-08 17:13:17 -0300

    simple java template

---
+++ templates/template.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Foo {
+    public static voic main(String args[]) {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        out.close();
+    }
+}
