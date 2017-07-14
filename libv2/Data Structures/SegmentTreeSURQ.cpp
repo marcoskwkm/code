f5b30cdd57f483b44bf665b25bea5ae50d05726f
Marcos Kawakami <marcoskwkm@gmail.com>
2017-07-14 11:11:04 -0300

    [libv2] Slightly improve segtree usability

--- libv2/Data Structures/SegmentTreeSURQ.cpp
+++ libv2/Data Structures/SegmentTreeSURQ.cpp
@@ -1,8 +1,9 @@
 // Single update range query segtree
 struct SegmentTree {
+    // In principle, it should be enough to change only this struct for any application.
     struct Node {
         lint sum, best_l, best_r, best;
-        Node() : sum(0), best_l(0), best_r(0), best(0) {}
+        Node(lint v = 0) : sum(v), best_l(v), best_r(v), best(v) {}
         Node(lint s, lint l, lint r, lint b) : sum(s), best_l(l), best_r(r), best(b) {}
 
         // Merge two nodes
@@ -14,6 +15,7 @@ struct SegmentTree {
                 max(best_r + that.best_l, max(best, that.best)));
         }
     };
+    const Node NEUTRAL = Node(0, -LINF, -LINF, -LINF);
 
     vector<Node> tree;
     int n;
@@ -24,24 +26,24 @@ struct SegmentTree {
     }
 
     Node build(int i, int l, int r, const vector<int> &v) {
-        if (l == r) return tree[i] = Node(v[l], v[l], v[l], v[l]);
+        if (l == r) return tree[i] = Node(v[l]);
         int L = 2*i + 1, R = 2*i + 2, mid = (l + r) / 2;
         return tree[i] = build(L, l, mid, v) + build(R, mid + 1, r, v);
     }
 
     Node update(int i, int l, int r, int x, int val) {
         if (x < l || x > r) return tree[i];
-        if (l == r) return tree[i] = Node(val, val, val, val);
+        if (l == r) return tree[i] = Node(val);
         int L = 2*i + 1, R = 2*i + 2, mid = (l + r) / 2;
         return tree[i] = update(L, l, mid, x, val) + update(R, mid + 1, r, x, val);            
     }
     Node update(int x, int val) { return update(0, 0, n - 1, x, val); }
 
     Node query(int i, int l, int r, int ql, int qr) {
-        if (qr < l || r < ql) return Node(0, -LINF, -LINF, -LINF);
+        if (qr < l || r < ql) return NEUTRAL;
         if (ql <= l && r <= qr) return tree[i];
         int L = 2*i + 1, R = 2*i + 2, mid = (l + r) / 2;
         return query(L, l, mid, ql, qr) + query(R, mid + 1, r, ql, qr);
     }
     Node query(int l, int r) { return query(0, 0, n - 1, l, r); }
-};    
+};
