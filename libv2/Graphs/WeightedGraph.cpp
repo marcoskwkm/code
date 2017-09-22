bbbba590cb4c0692bd7c1ddf2e81db30e944daee
Marcos Kawakami <marcoskwkm@gmail.com>
2017-09-22 19:07:49 -0300

    [libv2] Add Gomory-Hu tree. Needs testing.

---
+++ libv2/Graphs/WeightedGraph.cpp
@@ -0,0 +1,10 @@
+template<class WTYPE> struct WeightedGraph {
+    int V;
+    vector<vector<pair<int, WTYPE>>> adj;
+
+    WeightedGraph(int _V) : V(_V) { adj.resize(V); }
+
+    void add_edge(int u, int v, WTYPE w) {
+        adj[u].push_back(make_pair(v, w));
+    }
+};
