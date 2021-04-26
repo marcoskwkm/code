b5c50d8074fc748cf1113a7c574f470dcbc961e1
Marcos Kawakami <marcoskwkm@gmail.com>
2021-04-26 02:55:28 -0300

    Update C++ template

--- templates/template.cpp
+++ templates/template.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 #define debug(args...) fprintf(stderr, args)
-#define readIntArray(v, n) for_each(v.begin(), v.begin() + n, [](int &x) { scanf("%d", &x); })
-#define readLintArray(v, n) for_each(v.begin(), v.begin() + n, [](lint &x) { scanf("%lld", &x); })
+#define readArray(v, n) for_each(v.begin(), v.begin() + n, [](auto &x) { cin >> x; })
 
 typedef long long lint;
 typedef pair<int, int> pii;
@@ -13,7 +12,16 @@ typedef tuple<int, int, int> tiii;
 const int INF = 0x3f3f3f3f;
 const lint LINF = 0x3f3f3f3f3f3f3f3fll;
 
+template <class T, class C = vector<T>> inline void printArray(C v, int n = -1) {
+    int cnt = 0;
+    for (auto it = v.begin(); it != v.end() && cnt != n; it++, cnt++) {
+        cout << *it << (next(it) == v.end() || cnt + 1 == n ? '\n' : ' ');
+    }
+}
+
 int main() {
-    ;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
     return 0;
 }
