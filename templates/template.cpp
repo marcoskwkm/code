e9a6b801a714d41577a56dc4d562b0ce6eb62b04
Marcos Kawakami <marcoskwkm@gmail.com>
2021-05-08 07:13:26 -0300

    Add y combinator to C++ template

--- templates/template.cpp
+++ templates/template.cpp
@@ -12,7 +12,22 @@ typedef tuple<int, int, int> tiii;
 const int INF = 0x3f3f3f3f;
 const lint LINF = 0x3f3f3f3f3f3f3f3fll;
 
-template<class T = int> T read() {
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+
+  public:
+    template<class T> explicit y_combinator_result(T &&fun) : fun_(forward<T>(fun)) {}
+
+    template<class... Args> decltype(auto) operator()(Args &&...args) {
+        return fun_(ref(*this), forward<Args>(args)...);
+    }
+};
+
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) {
+    return y_combinator_result<decay_t<Fun>>(forward<Fun>(fun));
+}
+
+template<class T = int> inline T read() {
     T x;
     cin >> x;
     return x;
@@ -21,8 +36,9 @@ template<class T = int> T read() {
 template<class T, class C = vector<T>> inline void printArray(C v, int n = -1) {
     int cnt = 0;
     for (auto it = v.begin(); it != v.end() && cnt != n; it++, cnt++) {
-        cout << *it << (next(it) == v.end() || cnt + 1 == n ? '\n' : ' ');
+        cout << *it << (next(it) == v.end() || cnt + 1 == n ? "" : " ");
     }
+    cout << '\n';
 }
 
 int main() {
