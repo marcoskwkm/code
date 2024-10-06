342f61de7971e603ab843357908634c737ac5b38
Marcos Kawakami <marcoskwkm@gmail.com>
2024-10-05 21:32:47 -0300

    Use C++23 and remove y_combinator
    Recursive lambdas can now be written as
    
    auto foo = [&](this const auto &f, int n) -> int { ... }

--- templates/template.cpp
+++ templates/template.cpp
@@ -12,21 +12,6 @@ typedef tuple<int, int, int> tiii;
 const int INF = 0x3f3f3f3f;
 const lint LINF = 0x3f3f3f3f3f3f3f3fll;
 
-template<class Fun> class y_combinator_result {
-    Fun fun_;
-
-  public:
-    template<class T> explicit y_combinator_result(T &&fun) : fun_(forward<T>(fun)) {}
-
-    template<class... Args> decltype(auto) operator()(Args &&...args) {
-        return fun_(ref(*this), forward<Args>(args)...);
-    }
-};
-
-template<class Fun> decltype(auto) y_combinator(Fun &&fun) {
-    return y_combinator_result<decay_t<Fun>>(forward<Fun>(fun));
-}
-
 template<class T = int> inline T read() {
     T x;
     cin >> x;
